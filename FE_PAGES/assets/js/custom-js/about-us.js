9e0870e584c1bc58862edfbaa0195b5bc129d6fa
dayang4321 <dayang4321@users.noreply.github.com>
2020-08-01 08:32:33 +0100

    comments complete

--- FE_PAGES/assets/js/custom-js/about-us.js
+++ FE_PAGES/assets/js/custom-js/about-us.js
@@ -1,9 +1,17 @@
-$(document).ready(function(){
+
+
+//initialize wow.js for animation on scroll
+$( document ).ready(function() {
+    new WOW().init();
+});
+
+//initialize and Configure Slick Carousel
+$(document).ready(function () {
     $('.sponsors-icon-row').slick({
       slidesToShow: 8,
       slidesToScroll: 1,
       autoplay: true,
-      autoplaySpeed: 2000,
+      autoplaySpeed: 2500,
       arrows: false,
       dots: false,
       pauseOnHover: false,
